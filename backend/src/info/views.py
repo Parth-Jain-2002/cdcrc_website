51a3c7fbb9a1d5dfc93d9c74837953e51fa02064
TheViking733n <anant733n@gmail.com>
2023-05-27 20:50:45 +0530

    Fixed Student team endpoint, read data from sheets

--- backend/src/info/views.py
+++ backend/src/info/views.py
@@ -13,8 +13,9 @@ from utils.mailer import Mailer
 from utils.metadata import CDCRC_MEDIA_EMAIL, PRIMARY_ALERT_EMAILS, PRIMARY_PHONE
 from django.db import transaction
 from django.contrib.auth.decorators import login_required
+from .team_member import TeamMember
+from django.conf import settings
 import requests
-
 
 # Create your views here.
 def home(request):
@@ -81,19 +82,49 @@ def events_detail(request, pk):
     events = Events.objects.get(pk=pk)
     return render(request,'info/events_detail.html',{'events':events})
 
+def get_team_data(sheet_url):
+    """
+    Returns a list of TeamMember objects
+    Fetches data from Google Sheets
+    """
+    def parse_sheet_url(sheet_url):
+        """Returns the sheet_id and gid from the sheet_url"""
+        sheet_id = sheet_url.split('/')[5]
+        gid = sheet_url.split('/')[6].split('=')[1]
+        return sheet_id, gid
+
+    sheet_id, gid = parse_sheet_url(sheet_url)
+
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=tsv&gid={gid}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception('Error fetching data from Google Sheets')
+    data = response.text
+    members = []
+    for line in data.split('\n')[1:]:
+        line = line.strip()
+        if not line:
+            continue
+        fname, lname, email, designation, phone, linkedin = (line.split('\t') + ['']*6)[:6]
+        members.append(TeamMember(fname, lname, email, designation, phone, linkedin))
+    return members
 
 def faculty_team(request):
-    faculty_team_members = TeamMemberProfile.objects.filter((Q(category='faculty') | Q(category='staff')) & Q(visible=True)).order_by('-level')
-    members_remainder_4 = faculty_team_members.count()
+    # faculty_team_members = TeamMemberProfile.objects.filter((Q(category='faculty') | Q(category='staff')) & Q(visible=True)).order_by('-level')
+    # members_remainder_4 = faculty_team_members.count()
+    faculty_team_members = get_team_data(settings.FACULTY_MEMBERS_SHEET)
+    members_remainder_4 = len(faculty_team_members)
     context = {'team_members': faculty_team_members, 'team_members_remainder_4': range(1, members_remainder_4+1), 'title': 'Faculty Team', 'show_email': True}
     return render(request, 'info/team.html', context=context)
 
 def student_team(request):
-    return render(request, 'under_construction.html')
+    # return render(request, 'under_construction.html')
     # student_team_members = TeamMemberProfile.objects.filter((Q(category='ug') | Q(category='pg')) & Q(visible=True)).order_by('-level')
     # members_remainder_4 = student_team_members.count()
-    # context = {'team_members': student_team_members, 'team_members_remainder_4': range(1, members_remainder_4+1), 'title': 'Student Team', 'show_email': False}
-    # return render(request, 'info/team.html', context=context)
+    student_team_members = get_team_data(settings.STUDENT_MEMBERS_SHEET)
+    members_remainder_4 = len(student_team_members)
+    context = {'team_members': student_team_members, 'team_members_remainder_4': range(1, members_remainder_4+1), 'title': 'Student Team', 'show_email': False}
+    return render(request, 'info/team.html', context=context)
 
 def under_construction(request):
     return render(request, 'under_construction.html')
@@ -138,28 +169,6 @@ def contact_us_form(request):
 
 
 def placement_stats(request):
-
-    url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR04GjBFXPup1OhiDk4OH_kDReVpsKBs8nKYIo_sPo6oNofPq10R8IHo6hb3f6FmN7W1QXLbeiZb_ee/pub?output=tsv'
-
-    req = requests.get(url).text
-
-    allItems = []
-    item = []
-    for data_string in req.split('\n'):
-        if(data_string.startswith('NEW')):
-            if(item): # adding item to allItems if item is not empty
-                allItems.append(item)
-            item=[] # clearing item for new values
-            continue
-
-        # removing \r from the end of each data value
-        data_values = [data.rstrip('\r') for data in data_string.split('\t')]
-        item.append(data_values) # adding data_values to item
-
-    # add the last item
-    if(item):
-        allItems.append(item)
-    
-    return render(request, 'info/placement_stats.html', {'allItems': allItems})
+    return render(request, 'info/placement_stats.html')
 
 
